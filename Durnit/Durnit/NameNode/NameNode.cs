d19787df9a715a2bef21145957c2d1d550b7fd6a
Elijah Segura <elijahsegurar@gmail.com>
2015-12-08 10:31:07 -0700

    Reconfiguration of NameNode

--- Durnit/Durnit/NameNode/NameNode.cs
+++ Durnit/Durnit/NameNode/NameNode.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using System;
 using System.Collections.Generic;
 using System.Collections.Specialized;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Text;
@@ -30,16 +33,41 @@ namespace Durnit
             HttpListenerRequest request = context.Request;
             NameValueCollection requestHeaders = context.Request.Headers;
             HttpListenerResponse response = context.Response;
-            
+
+
             string durnitOp = requestHeaders.Get("X-DurnitOp");
+            JsonSerializer serializer = new JsonSerializer();
+
             switch(durnitOp)
             {
                 case "GetDatanodes":
+                    serializer.Converters.Add(new JavaScriptDateTimeConverter());
+                    serializer.NullValueHandling = NullValueHandling.Ignore;
 
+                    DataNodeInfo[] nodesToSend = GetDataNodes();
+
+                    using (StreamWriter sw = new StreamWriter(response.OutputStream))
+                    using (JsonWriter writer = new JsonTextWriter(sw))
+                    {
+                        foreach (DataNodeInfo info in nodesToSend)
+                        {
+                            serializer.Serialize(writer, info.URIAdress);
+                        }
+                        // {"ExpiryDate":new Date(1230375600000),"Price":0}
+                    }
                     response.StatusCode = 200;
                     break;
                 case "Heartbeat":
-                    //List<string> data = (List<string>)request.InputStream;
+                    DataNodeInfo sentInfo;
+                    using (StreamReader reader = new StreamReader(request.InputStream))
+                    using (JsonReader JsonRead = new JsonTextReader(reader))
+                    {
+                        sentInfo = (DataNodeInfo)serializer.Deserialize(JsonRead, typeof(DataNodeInfo));
+                    }
+
+                    DataNodeInfo correspondingInfo = log.FirstOrDefault(x => x.ID == sentInfo.ID);
+                    correspondingInfo.Files = sentInfo.Files;
+
                     response.StatusCode = 200;
                     break;
                 default:
@@ -50,7 +78,12 @@ namespace Durnit
 
             response.Close();
 
-            Console.WriteLine("got it!");
+            //Console.WriteLine("got it!");
+        }
+
+        private DataNodeInfo[] GetDataNodes()
+        {
+            throw new NotImplementedException();
         }
     }
 }
